6a2e7a9ccce2cad83e17bd8bddf4269e0f222d10
Andreia <andreia.machion@saojudas.br>
2022-04-03 17:51:01 -0300

    crud-usuario ate listagem 2.5.9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button'
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './cliente-inserir/cliente-inserir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ClienteInserirComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
